3e58c6796cba17b48ecf086454a40b08802c40cf
rconjoe <joe@rconjoe.com>
2021-12-20 21:24:36 -0800

    main

---
+++ src/main/java/chat/kassandra/discord_bot/DiscordBotApplication.java
@@ -0,0 +1,13 @@
+package chat.kassandra.discord_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordBotApplication.class, args);
+	}
+
+}
