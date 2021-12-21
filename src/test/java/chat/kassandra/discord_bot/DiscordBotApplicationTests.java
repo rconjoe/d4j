0ea414ca819832d1e439612e1d255f5b0028960b
rconjoe <joe@rconjoe.com>
2021-12-20 21:24:51 -0800

    unused tests rn

---
+++ src/test/java/chat/kassandra/discord_bot/DiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package chat.kassandra.discord_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
